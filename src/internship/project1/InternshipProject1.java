bfc57372b9d3ce3f6d1f116d47d88ab3f1a851a4
Legokiller <commanderpip@gmail.com>
2020-04-10 14:12:27 -0400

    4/10/2020 Update 10
    Small comment modification to include author names.

--- src/internship/project1/InternshipProject1.java
+++ src/internship/project1/InternshipProject1.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package internship.project1;
 
 import javafx.application.Application;
@@ -12,8 +7,8 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 /**
- *
- * @author Legop
+ * @name 3D Lab Internship Project
+ * @author James Floyd, Jared Smith
  */
 public class InternshipProject1 extends Application {
     
